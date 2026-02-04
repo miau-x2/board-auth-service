5829ef9c3305d6dec90c183e9128379d24abe467
miau-x2 <joined260130@gmail.com>
2026-02-05 01:40:19 +0900

    test: EmailSender 테스트 코드 작성

---
+++ src/test/java/com/example/board/auth/mail/service/EmailSenderTest.java
@@ -0,0 +1,150 @@
+package com.example.board.auth.mail.service;
+
+import com.example.board.auth.mail.MailProperties;
+import com.example.board.auth.mail.dto.MailContext;
+import com.example.board.auth.mail.exception.MailSendFailedException;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.retry.RetryPolicy;
+import org.springframework.core.retry.RetryTemplate;
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.MailPreparationException;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class EmailSenderTest {
+    @Mock
+    private JavaMailSender javaMailSender;
+    private MailProperties mailProperties;
+    private RetryTemplate retryTemplate;
+    private EmailSender emailSender;
+
+    @BeforeEach
+    void setUp() {
+        mailProperties = new MailProperties(new MailProperties.From("address", "service-name"));
+        var retryPolicy = RetryPolicy.builder()
+                .maxRetries(2)
+                .delay(Duration.ZERO)
+                .includes(MailSendFailedException.class)
+                .build();
+        retryTemplate = new RetryTemplate(retryPolicy);
+        emailSender = new ThrottledEmailSender(javaMailSender, mailProperties, retryTemplate);
+    }
+
+    @Test
+    @DisplayName("메일 전송 - 성공")
+    void send_success() {
+        var mailContext = new MailContext("user@example.com", "제목", "123456", 5);
+        var text = "내용";
+        var message = mock(MimeMessage.class);
+        when(javaMailSender.createMimeMessage()).thenReturn(message);
+
+        emailSender.send(mailContext, text);
+
+        verify(javaMailSender).createMimeMessage();
+        verify(javaMailSender).send(message);
+    }
+
+    @Test
+    @DisplayName("메일 전송 - 성공(째시도 가능 예외)")
+    void send_success_when_throw_mail_send_failed_exception() {
+        var mailContext = new MailContext("user@example.com", "제목", "123456", 5);
+        var text = "내용";
+        var message = mock(MimeMessage.class);
+        when(javaMailSender.createMimeMessage()).thenReturn(message);
+        doThrow(MailSendException.class)
+                .doThrow(MailSendException.class)
+                .doNothing()
+                .when(javaMailSender)
+                .send(message);
+
+        emailSender.send(mailContext, text);
+
+        verify(javaMailSender, times(3)).createMimeMessage();
+        verify(javaMailSender, times(3)).send(message);
+    }
+
+    @Test
+    @DisplayName("메일 전송 - 실패(재시도 불가능 예외 1)")
+    void send_fail_when_throw_mail_parse_exception() {
+        var mailContext = new MailContext("user@example.com", "제목", "123456", 5);
+        var text = "내용";
+        var message = mock(MimeMessage.class);
+        when(javaMailSender.createMimeMessage()).thenReturn(message);
+        doThrow(MailParseException.class)
+                .when(javaMailSender)
+                .send(message);
+
+        assertThatThrownBy(() -> emailSender.send(mailContext, text))
+                        .isExactlyInstanceOf(MailSendFailedException.class);
+
+        verify(javaMailSender).createMimeMessage();
+        verify(javaMailSender).send(message);
+    }
+
+    @Test
+    @DisplayName("메일 전송 - 실패(재시도 불가능 예외 2)")
+    void send_fail_when_throw_mail_preparation_exception() {
+        var mailContext = new MailContext("user@example.com", "제목", "123456", 5);
+        var text = "내용";
+        var message = mock(MimeMessage.class);
+        when(javaMailSender.createMimeMessage()).thenReturn(message);
+        doThrow(MailPreparationException.class)
+                .when(javaMailSender)
+                .send(message);
+
+        assertThatThrownBy(() -> emailSender.send(mailContext, text))
+                .isExactlyInstanceOf(MailSendFailedException.class);
+
+        verify(javaMailSender).createMimeMessage();
+        verify(javaMailSender).send(message);
+    }
+
+    @Test
+    @DisplayName("메일 전송 - 실패(재시도 불가능 예외 3)")
+    void send_fail_when_throw_mail_authentication_exception() {
+        var mailContext = new MailContext("user@example.com", "제목", "123456", 5);
+        var text = "내용";
+        var message = mock(MimeMessage.class);
+        when(javaMailSender.createMimeMessage()).thenReturn(message);
+        doThrow(MailAuthenticationException.class)
+                .when(javaMailSender)
+                .send(message);
+
+        assertThatThrownBy(() -> emailSender.send(mailContext, text))
+                .isExactlyInstanceOf(MailSendFailedException.class);
+
+        verify(javaMailSender).createMimeMessage();
+        verify(javaMailSender).send(message);
+    }
+
+    @Test
+    @DisplayName("메일 전송 - 실패(째시도 횟수 소진)")
+    void send_fail_when_retry_exhausted() {
+        var mailContext = new MailContext("user@example.com", "제목", "123456", 5);
+        var text = "내용";
+        var message = mock(MimeMessage.class);
+        when(javaMailSender.createMimeMessage()).thenReturn(message);
+        doThrow(MailSendException.class)
+                .when(javaMailSender)
+                .send(message);
+
+        assertThatThrownBy(() -> emailSender.send(mailContext, text))
+                .isExactlyInstanceOf(MailSendFailedException.class);
+
+        verify(javaMailSender, times(3)).createMimeMessage();
+        verify(javaMailSender, times(3)).send(message);
+    }
+}
---
+++ src/test/java/com/example/board/auth/mail/service/EmailServiceTest.java
@@ -0,0 +1,7 @@
+package com.example.board.auth.mail.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailServiceTest {
+
+}
