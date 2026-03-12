00804e99027df78d71b2de4f6f016c1634d98ad2
miau-x2 <joined260130@gmail.com>
2026-03-12 17:51:13 +0900

    refactor: DatabaseConstraintName클래스의 내부 정적 클래스 MemberCredential 생성자 private으로 변경

--- src/main/java/com/example/board/auth/commons/utils/DatabaseConstraintName.java
+++ src/main/java/com/example/board/auth/commons/utils/DatabaseConstraintName.java
@@ -4,6 +4,7 @@ public final class DatabaseConstraintName {
     private DatabaseConstraintName() {}
 
     public static final class MemberCredential {
+        private MemberCredential() {}
         public static final String EMAIL = "uk_member_credential_email";
         public static final String USERNAME = "uk_member_credential_username";
     }
